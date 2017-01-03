65c3464037d14733e407b8e2aa63d1607f0474c8
scohen18 <sbcohen2000@gmail.com>
2017-01-03 15:21:58 -0700

    Addition
    added some lines

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//index.js
+var unirest = require('unirest');
+var express = require('express');
+
+var app = express();
+app.set('views', '.');
+app.set("view engine", "pug");
+app.use(express.static('public'));
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+app.get("/data",function(req,res){
+    var inputTicker = req.query.inputTicker;
+    chart(inputTicker, function(response) {
+        res.json(response.raw_body);
+    })
+});
