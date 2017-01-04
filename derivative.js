86b515390d49bb0876306f9a52a5c8b1c6c5c76c
lmelendez17 <lmelendez17@kentdenver.org>
2017-01-03 23:37:20 -0700

    Addition
    Started the derivative. Very hard. needs alot of work

--- derivative.js
+++ derivative.js
@@ -1,2 +1,16 @@
 // check this link out
 //http://stackoverflow.com/questions/6399777/looking-for-derivative-script
+
+var inputFunction = "x^2+3";
+var parsedFunction = inputFunction.split("+");
+console.log(parsedFunction);
+// x^2  derivative = 2x
+for(i=0;i<parsedFunction.length;i++){
+  var finalParse = parsedFunction[i].split("^");
+//  for(j=0; j< finalParse.length;i++){
+
+console.log(finalParse);
+
+  //}
+
+}
