1a413433833c9be2f88c65c2f65c65af9f6cef3a
lmelendez17 <lmelendez17@kentdenver.org>
2017-01-09 11:32:51 -0700

    Updates
    just some minor changes to make them easier to use

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,12 +1,12 @@
 var Crawler = require('js-crawler');
 
-var urls = ["https://github.com", "https://www.nytimes.com"]
+var urls = ["https://www.kentdenver.org/"]
 for (var i = 0; i<urls.length; i++){
-new Crawler().configure({depth: 2})// we are just looking to get the article code
+new Crawler().configure({depth: 3})// we are just looking to get the article code
   .crawl(urls[i], function onSuccess(page) {
-    console.log(page.url);
+    //console.log(page.url);
   }, null, function onAllFinished(crawledUrls) {
     console.log('All crawling finished');
-    //console.log(crawledUrls); // we want to store this value
+    console.log(crawledUrls); // we want to store this value
   });
 }
--- derivative.js
+++ derivative.js
@@ -1,13 +1,12 @@
 var derivative = require('math-lib');
+var output;
 
-function takeDerivative(equation, xValue){
-
-console.log('foo1', derivative().computeSecondDerivative(function(x) {
-  return equation
-  return x*x;
-}, xValue));
-}
-
+//function takeDerivative(equation, xValue){
+output = ('foo1', derivative().computeSecondDerivative(function(x) {
+  return x*x*x*x;
+}, 2));
+//}
+console.log(output);
 // console.log('foo1', derivative().computeSecondDerivative(function(x) {
 //   return x*x;
 // }, 4));
