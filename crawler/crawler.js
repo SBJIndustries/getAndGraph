86192f5b1551177ff4773d5a345f196a8a98b008
lmelendez17 <lmelendez17@kentdenver.org>
2017-01-12 19:26:21 -0700

    addition
    I added the part where it only searches the base link for the code. It
    still needs to work because i got it check but not remove the url from
    the array.

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,38 +1,53 @@
 var Crawler = require('js-crawler');
 var colors = require('colors');
 
-var urls = ["http://www.wsj.com/"];
+var urls = ["http://www.kentdenver.org/"];
 var rawHtml;
 var pageCounter = 0;
 var pages = [];
 var plainText;
 
-function crawl(cb)
-{
-	for (var i = 0; i<urls.length; i++){
+function crawl(cb) {
+    for (var i = 0; i < urls.length; i++) {
+      //console.log(urls[i].length)
+        new Crawler().configure({
+                depth: 3
+            }) // we are just looking to get the article code
+            .crawl(urls[i], function onSuccess(page) {
+                pageCounter++;
+                //console.log(pageCounter);
+                pages.push(page.content);
+            }, null, function onAllFinished(crawledUrls) {
+                for(var i = 0; i< urls.length; i++){
+                  for(var j = 0; j < crawledUrls.length;j++){
+                    if(urls[i]==crawledUrls[j].slice(0,urls[i].length)){
+                      crawledUrls.splice( j, 1 );
+                    }
+                  }
 
-		new Crawler().configure({depth: 2})// we are just looking to get the article code
-	  	.crawl(urls[i], function onSuccess(page) {
-			pageCounter ++;
-			console.log(pageCounter);
-	    		pages.push(page.content);
-	  	}, null, function onAllFinished(crawledUrls) {
+                }
 
-	    		console.log('All crawling finished');
-			cb(rawHtml);
-	    		//console.log(crawledUrls); // we want to store this value
 
-	  });
-	}
+
+                //console.log(crawledUrls[j]); // we want to store this value
+                //cb(rawHtml);
+
+
+                console.log(crawledUrls);
+
+
+                console.log('All crawling finished');
+            });
+    }
 }
 
-crawl(function(rawHtml) {
-	for(var i = 0; i < pages.length; i ++)
-	{
-		var cheerio = require('cheerio'),
-			$ = cheerio.load(pages[i]);
-		plainText += $('body').text();
 
-	}
-	console.log(plainText);
+crawl(function(rawHtml) {
+    for (var i = 0; i < pages.length; i++) {
+        var cheerio = require('cheerio'),
+            $ = cheerio.load(pages[i]);
+        plainText += $('body').text();
+
+    }
+    //	console.log(plainText);
 });
