4b9b87495a25c8d821eb8b6e9599fcf3f2340e65
scohen18 <sbcohen2000@gmail.com>
2017-01-16 15:51:09 -0700

    addition
    fixed terms program

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -2,7 +2,7 @@ var Crawler = require('js-crawler');
 var colors = require('colors');
 
 var urls = ["http://www.cnet.com"];
-var terms = ["news"]
+var terms = ["news","apple"];
 var rawHtml;
 var pageCounter = 0;
 var pages = [];
@@ -13,10 +13,10 @@ function crawl(cb) {
     for (let i = 0; i < urls.length; i++) {
 
         new Crawler().configure({
-                depth: 3
+                depth: 2
             })
             .crawl(urls[i], function onSuccess(page) {
-                if (page.url.search(urls[i].slice(11,urls[i].length-5))>0 && page.url.search(terms[i])>0){// this just checks if main part of the urls is in the searched urls
+                if (page.url.search(urls[i].slice(11,urls[i].length-5))>0 && new RegExp(terms.join("|")).test(page.url)){// this just checks if main part of the urls is in the searched urls
                     console.log(page.url);                                                    //this i needs to be changed to a j that loops through the entire terms list everytime a url is searched
                     pages.push(page.content);
                     pageCounter++;
@@ -34,7 +34,8 @@ crawl(function(pagesIn) {
     for (var i = 0; i < pagesIn.length; i++) {
         var cheerio = require('cheerio'),
             $ = cheerio.load(pagesIn[i]);
-        plainText += $('body').text();
+        plainText += $('article').text();
+	   console.log(plainText);
 
     }
 });
