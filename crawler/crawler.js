eb4effbb64a158ea36266b88eb181e4d2a59f85a
lmelendez17 <lmelendez17@kentdenver.org>
2017-01-16 14:31:18 -0700

    addition
    improved the search so it goes for key terms and not just exact url.
    This will give us a wider base to look at.

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,7 +1,8 @@
 var Crawler = require('js-crawler');
 var colors = require('colors');
 
-var urls = ["http://www.kentdenver.org/"];
+var urls = ["http://www.cnet.com"];
+var terms = ["news"]
 var rawHtml;
 var pageCounter = 0;
 var pages = [];
@@ -12,17 +13,18 @@ function crawl(cb) {
     for (let i = 0; i < urls.length; i++) {
 
         new Crawler().configure({
-                depth: 2
+                depth: 3
             })
             .crawl(urls[i], function onSuccess(page) {
-                if (urls[i] == page.url.slice(0, urls[i].length)) {
-                    console.log(page.url);
+                if (page.url.search(urls[i].slice(11,urls[i].length-5))>0 && page.url.search(terms[i])>0){// this just checks if main part of the urls is in the searched urls
+                    console.log(page.url);                                                    //this i needs to be changed to a j that loops through the entire terms list everytime a url is searched
                     pages.push(page.content);
                     pageCounter++;
                 }
             }, null, function onAllFinished(crawledUrls) {
                 cb(pages);
                 console.log('Done'.red);
+
             });
     }
 }
