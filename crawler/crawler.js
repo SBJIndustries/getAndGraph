0ebd1497012f938ccdbae981598977b2109073d4
lmelendez17 <lmelendez17@kentdenver.org>
2017-01-16 20:35:41 -0700

    addition
    I just added a comment about something we may want to consider. I am
    going to investigate it...

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -15,11 +15,11 @@ function crawl(cb) {
     for (let i = 0; i < urls.length; i++) {
 
         new Crawler().configure({
-                depth: 2
+                depth: 2// maybe we want to vary the depth depending on what the program finds
             })
             .crawl(urls[i], function onSuccess(page) {
                 if (page.url.search(urls[i].slice(11,urls[i].length))>0 || (termsOn && new RegExp(terms.join("|")).test(page.url))){// this just checks if main part of the urls is in the searched urls
-                    console.log(page.url);                                                    //this i needs to be changed to a j that loops through the entire terms list everytime a url is searched
+                    console.log(page.url);
                     pages.push(page.content);
                     pageCounter++;
                 }
@@ -30,7 +30,6 @@ function crawl(cb) {
             });
     }
 }
-
 
 crawl(function(pagesIn) {
     for (var i = 0; i < pagesIn.length; i++) {
