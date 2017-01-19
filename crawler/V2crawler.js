370bc2ce4dbf1ae7501c78797423ee69be8b847f
scohen18 <sbcohen2000@gmail.com>
2017-01-19 11:29:43 -0700

    added rich text
    it kind of works

--- crawler/V2crawler.js
+++ crawler/V2crawler.js
@@ -1,39 +1,39 @@
 var Crawler = require('js-crawler');
 var colors = require('colors');
 
-var urls = ["http://www.apple.com/"];
-var termsOn = true;
-var terms = ["mac"];
-var rawHtml;
+var urls = ["http://www.cnet.com/"];
+var termsOn = false;
+var terms = ["money"];
 var deep = 2;
 var pageCounter = 0;
 var pages = [];
-var plainText;
+var allContent = [];
+var plainText = [];
 var counter = 0;
 
 function crawl(cb) {
     for (let i = 0; i < urls.length; i++) {
 
         new Crawler().configure({
-
-                depth: 2// maybe we want to vary the depth depending on what the program finds
-
+                depth: 2
             })
             .crawl(urls[i], function onSuccess(page) {
                 if (page.url.search(urls[i].slice(11,urls[i].length-1))>0){
-								 	if (termsOn && new RegExp(terms.join("|")).test(page.url)){
-										urls.push(page.url);
-									}
-                    console.log(page.url);
-                    pages.push(page.content);
+			 	if (termsOn && new RegExp(terms.join("|")).test(page.url)){
+					urls.push(page.url);
+					allContent.push(page.content);
+				}
+				if(!termsOn){
+					urls.push(page.url);
+					allContent.push(page.content);
+				}
                     pageCounter++;
+				console.log(pageCounter);
                 }
             }, null, function onAllFinished(crawledUrls) {
-                //cb(pages);
-                //console.log(pageCounter)
-
                 console.log('Done'.blue);
-								console.log(urls)
+			console.log(urls)
+			cb(allContent);
 
             });
     }
@@ -43,8 +43,9 @@ crawl(function(pagesIn) {
     for (var i = 0; i < pagesIn.length; i++) {
         var cheerio = require('cheerio'),
             $ = cheerio.load(pagesIn[i]);
-        plainText += $('article').text();
-	   console.log(plainText);
-
+        plainText.push($('article p'));
+    }
+    for (var i = 0; i < plainText.length; i ++) {
+	    console.log(plainText[i].text());
     }
 });
